1984fc4f1737612e0090787770e158c9b21627a3
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-04-22 17:14:08 +0500

    feat: add state for login form

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,1 +1,16 @@
-export default class AuthService {}
+import ApiService from "@/network/axios";
+
+interface LoginPayload {
+  username: string;
+  password: string;
+}
+
+export default class AuthService {
+  static login(payload: LoginPayload) {
+    return ApiService.post("/auth/login", payload);
+  }
+
+  static getProfile() {
+    return ApiService.get("/auth/me");
+  }
+}
