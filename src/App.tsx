6645445d77bd6981e7debf89c09b92189da6739f
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-04-30 15:49:07 +0500

    feat: add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { RouterProvider } from "react-router";
 import { router } from "./router";
+import { Provider } from "react-redux";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import store from "./store";
 
 export default function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </QueryClientProvider>
   );
 }
--- src/views/main/home_view/HomePage.tsx
+++ src/views/main/home_view/HomePage.tsx
@@ -1,8 +1,14 @@
 import { getPosts } from "./api/get_posts";
 import { useQuery } from "@tanstack/react-query";
+import { RootState } from "@/store";
+import { useDispatch, useSelector } from "react-redux";
 import PostCard from "./components/post_card/PostCard";
+import { decrement, increment } from "@/store/slices/counter_slice";
 
 const HomePage = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
   const { data, isLoading } = useQuery({
     queryKey: ["get_posts"],
     queryFn: () => getPosts(),
@@ -11,6 +17,11 @@ const HomePage = () => {
   return (
     <>
       <h3 className='text-xl font-semibold mb-4'>Posts page</h3>
+      <h3>Counter {count}</h3>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
       {isLoading && (
         <div className='flex justify-center items-center'>Loading...</div>
       )}
