007a5a5eebf9fc61e3595bfb20feec48d484a9bf
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-05-05 16:36:30 +0500

    feat: create todo functionality

--- src/views/main/todos_view/TodosPage.tsx
+++ src/views/main/todos_view/TodosPage.tsx
@@ -1,18 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { getTodos } from "./api/get_todos";
 import { updateTodo } from "./api/update_todo";
+import { createTodo } from "./components/create_todo";
 import { setTodos, toggleTodo } from "@/store/slices/todos_slice";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import TodoItem from "./components/todo_item/TodoItem";
+import { Input } from "@/components/ui/input";
 
 const TodosPage = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state: RootState) => state.todo.todos);
+  const [isOpen, setIsOpen] = useState(false);
+  const [title, setTitle] = useState("");
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, refetch } = useQuery({
     queryKey: ["get_todos"],
     queryFn: () => getTodos(),
   });
@@ -21,6 +32,15 @@ const TodosPage = () => {
     mutationFn: ({ id, payload }: any) => updateTodo(id, payload),
     onSuccess: (data) => {
       dispatch(toggleTodo({ id: data.id, completed: data.completed }));
+    },
+  });
+
+  const { mutate: mutateCreateTodo, isPending: createLoading } = useMutation({
+    mutationFn: (payload: any) => createTodo(payload),
+    onSuccess: () => {
+      setIsOpen(false);
+      setTitle("");
+      refetch();
     },
   });
 
@@ -37,22 +57,55 @@ const TodosPage = () => {
     mutate({ id, payload });
   };
 
+  const onSubmitHandler = () => {
+    const payload = {
+      todo: title,
+      completed: false,
+      userId: localStorage.getItem("id"),
+    };
+    mutateCreateTodo(payload);
+  };
+
   return (
-    <div className='bg-white rounded-xl p-5 max-w-2xl mx-auto shadow-sm'>
-      <div className='flex items-center justify-between mb-5'>
-        <h1 className='text-xl  font-semibold text-center'>Todos</h1>
-        <Button>Add Todo</Button>
+    <>
+      <div className='bg-white rounded-xl p-5 max-w-2xl mx-auto shadow-sm'>
+        <div className='flex items-center justify-between mb-5'>
+          <h1 className='text-xl  font-semibold text-center'>Todos</h1>
+          <Button onClick={() => setIsOpen(true)}>Add Todo</Button>
+        </div>
+        {isLoading && <p className='text-center'>Loading...</p>}
+        {todos &&
+          todos.map((todo: any) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onChangeHandler={onChangeHandler}
+            />
+          ))}
       </div>
-      {isLoading && <p className='text-center'>Loading...</p>}
-      {todos &&
-        todos.map((todo: any) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            onChangeHandler={onChangeHandler}
-          />
-        ))}
-    </div>
+      <Dialog open={isOpen} onOpenChange={(open) => setIsOpen(open)}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create new todo</DialogTitle>
+          </DialogHeader>
+          <DialogDescription className='flex flex-col gap-4'>
+            <Input
+              className='h-[50px]'
+              placeholder='Enter todo title'
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <Button
+              disabled={createLoading || title.length === 0}
+              onClick={onSubmitHandler}
+              className='w-max ml-auto h-[50px]'
+            >
+              Create todo
+            </Button>
+          </DialogDescription>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
