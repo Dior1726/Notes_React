750b9c13f30fd9a4ae0a4db23c970b0e8e6425f5
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-05-06 11:27:10 +0500

    feat: create product list and card component

--- src/views/main/products_view/ProductsPage.tsx
+++ src/views/main/products_view/ProductsPage.tsx
@@ -1,5 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "./api/get_products";
+import ProductList from "./components/product_list/ProductList";
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  const { data, isLoading } = useQuery({
+    queryKey: ["get_products"],
+    queryFn: () => getProducts(),
+  });
+
+  return (
+    <>
+      {isLoading && <div className='text-center mt-10'>Loading...</div>}
+      {data && (
+        <div>
+          <h1 className='text-xl font-semibold mb-5'>Products</h1>
+          <ProductList data={data.products} />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ProductsPage;
